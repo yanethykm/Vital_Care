bd94ced925808e62a64be3d48dfa5b9f5bb6b506
unknown <yanethykm@gmail.com>
2023-11-06 18:49:46 -0500

    Añadiendo sql de los procedimientos almacenados de la tabla Vendedor

---
+++ SP_VENDEDOR.sql
@@ -0,0 +1,79 @@
+--------------------------------------------------------
+----------- PROCEDIMIENTO ALMACENADO INSERT ------------
+--------------------------------------------------------
+CREATE OR REPLACE PROCEDURE sp_Insert_Vendedor (
+    p_IdPersona IN INT
+) AS
+BEGIN
+    INSERT INTO Vendedor (IdPersona)
+    VALUES (p_IdPersona);
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END sp_Insert_Vendedor;
+/
+
+
+------------------------------------------------------------------------------
+--------- PROCEDIMIENTO ALMACENADO DELETE ------------------------------------
+------------------------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE sp_Delete_Vendedor (
+    p_ID IN INT
+) AS
+BEGIN
+    DELETE FROM Vendedor
+    WHERE ID = p_ID;
+    
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontró un vendedor con el ID ' || p_ID);
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END sp_Delete_Vendedor;
+/
+
+
+-------------------------------------------------------------------------------
+--------------- PROCEDIMIENTO ALMACENADO UPDATE -------------------------------
+-------------------------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE sp_Update_Vendedor (
+    p_ID IN INT,
+    p_IdPersona IN INT
+) AS
+BEGIN
+    UPDATE Vendedor
+    SET IdPersona = p_IdPersona
+    WHERE id = p_ID;
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontró un vendedor con ID ' || p_ID);
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END sp_Update_Vendedor;
+/
+-------------------------------------------------------------------------------
+--------------- PROCEDIMIENTO ALMACENADO LIST ---------------------------------
+-------------------------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE sp_List_Vendedor (
+    p_ID IN INT,
+    p_IdPersona OUT INT
+) AS
+BEGIN
+    SELECT IdPersona INTO p_IdPersona
+    FROM Vendedor
+    WHERE ID = p_ID;
+
+    EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontró un vendedor con el ID ' || p_ID);
+    WHEN OTHERS THEN
+        RAISE;
+END sp_List_Vendedor;
+/
+
