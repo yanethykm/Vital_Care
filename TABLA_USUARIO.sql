fdcd0f609aa0a76ebad1ae1298bc804d0c42e2ea
unknown <yanethykm@gmail.com>
2023-11-11 13:23:06 -0500

    Añadiendo SQL para la creación de la tabla Usuario con su respectivo disparador y secuencia.

--- TABLA_USUARIO.sql
+++ TABLA_USUARIO.sql
@@ -1,26 +1,28 @@
 ------------------------------------------/
---------- TABLA PROVEEDOR ----------------/
+--------- TABLA USUARIO ----------------/
 ------------------------------------------/
-CREATE TABLE Proveedor (
+CREATE TABLE Usuario (
     ID INT PRIMARY KEY,
+    Usuario VARCHAR(255),
+    Contraseña VARCHAR(40),
     IdPersona INT,
     FOREIGN KEY (IdPersona) REFERENCES Persona(ID)
 );
 
 ------------------------------------------------------------/
------ SECUENCIA PARA EL AUTOINCREMENTABLE ID PROVEEDOR -----/
+----- SECUENCIA PARA EL AUTOINCREMENTABLE ID USUARIO -----/
 ------------------------------------------------------------/
-CREATE SEQUENCE Proveedor_ID_Seq
+CREATE SEQUENCE Usuario_ID_Seq
   START WITH 1
   INCREMENT BY 1
   NOCACHE;
 
 -------- DISPARADOR PARA EL AUTOINCREMENTABLE -----/
-CREATE OR REPLACE TRIGGER Proveedor_BI_TRG
-BEFORE INSERT ON Proveedor
+CREATE OR REPLACE TRIGGER Usuario_BI_TRG
+BEFORE INSERT ON Usuario
 FOR EACH ROW
 BEGIN
-  SELECT Proveedor_ID_Seq.NEXTVAL
+  SELECT Usuario_ID_Seq.NEXTVAL
   INTO :NEW.ID
   FROM dual;
 END;
